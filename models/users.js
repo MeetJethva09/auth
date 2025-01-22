1d42e8bb08b87b7e68d0fa9efeac393c50d26698
themeetjethva <meetjethva1001@gmail.com>
2025-01-22 12:02:33 +0530

    databse version

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/authtest`)
+ const mongoUrl = mongoose.connect(`mongodb://127.0.0.1:27017/authtest`)
 
 let userSchema = mongoose.Schema({
     username : String,
